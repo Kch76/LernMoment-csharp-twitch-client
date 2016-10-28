49ada5d737102b5fd9b2a4f47e3974cb5c264fc5
Jan Suchotzki <jan@lernmoment.de>
2016-10-28 22:43:58 +0200

    Fügt weitere Test User und Channel Namen ein

--- ConsoleTwitchClient/Program.cs
+++ ConsoleTwitchClient/Program.cs
@@ -11,7 +11,9 @@ namespace ConsoleTwitchClient
     class Program
     {
         private static readonly string clientId = "865mxocrji3lctk8kscy3zgvkxm7t3e";
-        private static readonly string userName = "tutorialtestuser";
+        private static readonly string tutorialUserName = "tutorialtestuser";
+        private static readonly string manyFollowerUserName = "jonnyzocktyt";
+        private static readonly string largeChannelName = "RedCityBoy23";
 
         static void Main(string[] args)
         {
