9a65c7527b01d8a91b366b75e4cf3a8893f22256
Jan Suchotzki <jan@lernmoment.de>
2016-10-28 22:40:04 +0200

    Fügt usings und user informationen ein

--- ConsoleTwitchClient/Program.cs
+++ ConsoleTwitchClient/Program.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TwitchLib;
+using TwitchLib.TwitchAPIClasses;
 
 namespace ConsoleTwitchClient
 {
     class Program
     {
+        private static readonly string clientId = "865mxocrji3lctk8kscy3zgvkxm7t3e";
+        private static readonly string userName = "tutorialtestuser";
+
         static void Main(string[] args)
         {
         }
